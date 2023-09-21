80674b6ea60dffb1c5cef024dfc6a3b60be02684
Joel Alderholm <joel.alderholm@outlook.com>
2023-09-21 13:52:54 +0200

    ja

--- ye.py
+++ ye.py
@@ -1,2 +1,7 @@
-print("Hello World")
-print("rikardkarlsson.se")
+#Uppgift 1 Kasta Tärning
+
+import random
+def roll_dice():
+    return random.randint(1, 6)
+def play_game():
+    print("Vill du spela?")
